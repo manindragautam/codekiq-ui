52c905b8ea4be6008073b66abd3df9e484bf49d3
Manindra Gautam <manindragautamofficial@gmail.com>
2018-02-17 18:25:24 +0530

    initialized: boilerplate for react based npm

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+class Codekiq extends React.Component {
+  render () {
+    return (
+      <div className='text'>Codekiq UI</div>
+    )
+  }
+}
+export default Codekiq
